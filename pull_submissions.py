9fd9f34c7fe1e54487534f26f9c7e792da8b836f
Atul Varma <varmaa@gmail.com>
2011-02-27 17:28:25 -0800

    allow multiple periods to be specified in pull_submissions.py.

--- pull_submissions.py
+++ pull_submissions.py
@@ -5,7 +5,8 @@ import urllib
 import urllib2
 from optparse import OptionParser
 
-parser = OptionParser(usage="%prog [options] yyyy-mm username password")
+usage = "%prog [options] yyyy-mm,yyyy-mm,... username password"
+parser = OptionParser(usage=usage)
 parser.add_option("", "--chapter-id", dest="chapter_id",
                   default="4", help="chapter id (default is 4 for SF)")
 
@@ -17,26 +18,26 @@ if len(args) != 3:
     parser.print_help()
     sys.exit(1)
 
-(period, username, password) = args
+(periods, username, password) = args
 
-params = dict(period=period,
-              chapter_id=options.chapter_id,
-              exclude_unspecified='1')
-
-queryargs = urllib.urlencode(params)
-
-full_url = "%s?%s" % (url, queryargs)
+periods = periods.split(',')
+periods.reverse()
 
 passwords = urllib2.HTTPPasswordMgr()
 passwords.add_password('Application', url, username, password)
 handler = urllib2.HTTPBasicAuthHandler(passwords)
-
 opener = urllib2.build_opener(handler)
 
-response = opener.open(full_url)
+rows = []
 
-csvfile = response.read()
+for period in periods:
+    params = dict(period=period.strip(),
+                  chapter_id=options.chapter_id,
+                  exclude_unspecified='1')
+    queryargs = urllib.urlencode(params)
+    full_url = "%s?%s" % (url, queryargs)
+    response = opener.open(full_url)
+    reader = csv.DictReader(response)
+    rows.extend([row for row in reader])
 
-reader = csv.reader(csvfile.splitlines())
-rows = [row for row in reader]
 print json.dumps(rows, indent=2)
